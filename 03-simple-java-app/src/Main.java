e37c9882c309f6dbff3a00b350314673a52bb47a
Ubuntu <ubuntu@ip-172-31-34-144.ec2.internal>
2026-07-04 21:09:28 +0600

    added lab 03 - a simple java app

---
+++ 03-simple-java-app/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Sourav! Welcome to Docker!!! Current date: " + currentDate);
+    }
+}
